7516b7171b8d80f9313578de65c6bce623bfecaa
sumit89 <sumitkumarsuman11@gmail.com>
2020-05-02 16:21:24 +0530

    Updates

---
+++ precache-manifest.a7be35fe8c3a133a4b7baed554aa515d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80af97feb9c7b0a65080d8f123cd7034",
+    "url": "/MyPrivateReactRepository/contactmanagercontext/index.html"
+  },
+  {
+    "revision": "ae6c23b20509c1c4386f",
+    "url": "/MyPrivateReactRepository/contactmanagercontext/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "59ef212f4cfad9b3293d",
+    "url": "/MyPrivateReactRepository/contactmanagercontext/static/css/main.281a20d3.chunk.css"
+  },
+  {
+    "revision": "ae6c23b20509c1c4386f",
+    "url": "/MyPrivateReactRepository/contactmanagercontext/static/js/2.9345ad6a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MyPrivateReactRepository/contactmanagercontext/static/js/2.9345ad6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ef212f4cfad9b3293d",
+    "url": "/MyPrivateReactRepository/contactmanagercontext/static/js/main.2faf22f5.chunk.js"
+  },
+  {
+    "revision": "831ddda9abd30d6852c0",
+    "url": "/MyPrivateReactRepository/contactmanagercontext/static/js/runtime-main.7920aced.js"
+  }
+]);
